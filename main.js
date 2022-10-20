8f997767c7f56b8ba7dc5aaa427150962531de73
c0d3rMark <MarkM78@users.noreply.github.com>
2022-10-20 20:05:16 +1100

    Added blur effect for modal ,cleaned username input

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 'use strict';
 
-// let person = prompt('Enter your name for a more personal welcome:');
+// --------------------------------------------------------------------------
 
-// console.log(person);
+/* Dialog blur effect and user input to customise text */
+
+// Show dialog box for input. Blur background if open
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('userPrompt').show();
+    document.getElementById('userPrompt').showModal();
 });
 
-document.addEventListener('change', function() {
+// Add username if entered
+
+document.getElementById('userPrompt').addEventListener('change', function() {
     let userName = document.getElementById('name').value;
     let userGreeting = document.querySelectorAll('.yourName');
 
     for (let i in userGreeting) {
-        i < 1 ? userGreeting[0].innerText = userName 
-           : userGreeting[1].innerText = `${userName},`;
+        i < 1 ? userGreeting[0].innerText = `, ${userName}`
+           : i < 2 ? userGreeting[1].innerText = `${userName}, as`
+           : userGreeting[2].innerText = `${userName}, we`;
     }
 });
